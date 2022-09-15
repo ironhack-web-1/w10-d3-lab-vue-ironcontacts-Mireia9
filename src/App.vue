de3b4b44c5105ed728939a1b4915b42de627f678
Mireia Bertran <mbertran9@gmail.com>
2022-09-15 21:46:02 +0200

    Iteration 1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="app">
+    <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      <Contact
+        v-for="contactFor in firstFive"
+        :key="contactFor"
+        :contactFor="contactFor"
+      />
+    </table>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+import contacts from "./contacts.json";
+import Contact from "./components/Contact.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      contacts: contacts,
+      firstFive: [...contacts.slice(0, 5)],
+    };
+  },
+
+  components: { Contact },
+};
+</script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <tr>
+    <td><img :src="contactFor.pictureUrl" /></td>
+    <td>{{ contactFor.name }}</td>
+    <td>{{ contactFor.popularity.toFixed(2) }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+  props: {
+    contactFor: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style scoped>
+td {
+  height: 150px;
+  padding: 10px 50px;
+}
+
+img {
+  width: 5rem;
+}
+</style>
